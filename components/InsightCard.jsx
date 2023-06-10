8f369eb00e400b6addfb378e926a957b5a3c7c1a
Adrian <isaacona2@gmail.com>
2023-06-10 18:43:30 +0100

    Main project 8

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -8,20 +8,25 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
     variants={fadeIn('up', 'spring', index * 0.5, 1)}
     className="flex md:flex-row flex-col gap-4"
   >
-    <img
-      src={imgUrl}
-      alt="planet-01"
-      className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
-    />
+    <a href="">
+      <img
+        src={imgUrl}
+        alt="planet-01"
+        className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
+        />
+    </a>
     <div className="w-full flex justify-between items-center">
       <div className="flex-1 md-ml-[62px] flex flex-col max-w-[600px]">
-        <h4 className="font-normal lg:text-[42px] text-white">{title}</h4>
+        <a href="">
+          <h4 className="font-normal lg:text-[42px] text-white">{title}</h4>
+        </a>
         <p className="mt-[16px] font-normal lg:text-[20px] text-[14px] text-secondary-white">{subtitle}</p>
       </div>
-
-      <div className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white">
-        <img src="arrow.png" alt="arrow" className="w-[100%] object-contain " />
-      </div>
+      <a href="">
+        <div className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white">
+          <img src="arrow.png" alt="arrow" className="w-[100%] object-contain " />
+        </div>
+      </a>
     </div>
   </motion.div>
 );
